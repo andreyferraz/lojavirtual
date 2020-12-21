f6efd3d005f7ca91945590a7964c83c1c3080028
andreyferraz <andrey.developer@hotmail.com>
2020-12-21 15:58:52 -0300

    Admin - Login com erro de validação do usuário

--- index.php
+++ index.php
@@ -1,23 +1,52 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
+// rotas das páginas da loja 
 $app->get('/', function() {
 	$page = new Page();
 	$page->setTpl("index");
 });
 
-$app->get('/painel', function() {
+// rotas do painel admin da loja 
+$app->get('/admin', function() {
+	User::verifyLogin();
 	$page = new PageAdmin();
 	$page->setTpl("index");
+});
+
+// rota da tela de login da loja 
+$app->get('/admin/login', function(){
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("login");
+});
+
+//redirecionamento da tela de login
+$app->post('/admin/login', function(){
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+//rota para fazer o logout
+$app->get('/admin/logout', function(){
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
